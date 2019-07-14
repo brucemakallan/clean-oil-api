0ccccc5de209472e2c66300be914d42a168b06f7
Bruce Allan Makaaru <brucemakallan@gmail.com>
2019-07-14 17:50:39 +0300

    Initial Commit - Add API Dashboard template

---
+++ README.md
@@ -0,0 +1,6 @@
+# Clean Oil Uganda Ltd.
+
+### Developer Notes:
+- Documentation: https://docs.google.com/document/d/1h1yQWJV5Xy8v05WGnrP8h0vU7KDtJ0UWz22_sCmsNbg/edit?ts=5d28adc6
+- Trello: https://trello.com/b/f5EZsxqv/clean-oil
+
---
+++ src/api/utils.js
@@ -0,0 +1,9 @@
+const WELCOME_MESSAGE = 'Welcome to Clean Oil Uganda Ltd. API';
+const UNAUTHORIZED_ERROR_MESSAGE = 'You are not Authorized to perform this action';
+const PROTECTED_ENDPOINT_PREFIX = '/api/protected';
+
+module.exports = {
+	WELCOME_MESSAGE,
+	UNAUTHORIZED_ERROR_MESSAGE,
+	PROTECTED_ENDPOINT_PREFIX,
+};
